38447b367f5e47a6b91c07c73ab826e06f858233
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2019-08-09 12:25:11 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,9 @@
+year = int(input(" "))
+if year % 4 == 0 and year % 100 != 0:
+    print(" yes ")
+elif year % 100 == 0:
+    print( "No")
+elif year % 400 ==0:
+    print(" yes ")
+else:
+    print("no")
