7759e9a30a37448e2b49b696065d5a28e7773810
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2019-08-19 09:39:45 +0530

    Create pla18.py

---
+++ pla18.py
@@ -0,0 +1,15 @@
+#p
+from collections import Counter    
+string="kabali"
+
+n=int(input())                      
+values=[]
+count=0
+if  n>=1 and n<=1000:                
+    for i in range(n):
+        values.append((input()))
+
+for val in values:
+    if Counter(val)==Counter(string):     
+        count=count+1
+print(count)
